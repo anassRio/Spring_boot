fa045f702a0d78a51c6f6e209f95373e2b469127
anassRio <rouimi.anass@gmail.com>
2022-09-29 00:05:01 +0200

    Develop (#3)
    * Add Repository & DataBase
    
    * Post Mapping
    
    * Delete + Post Mapping
    
    * delte target folder
    
    * Feature/repository (#2)
    
    * Add Repository & DataBase
    
    * Post Mapping
    
    * Delete + Post Mapping
    
    * delete target folder
    
    * Tuto URL

--- src/main/java/com/rionass/demonstration/student/StudentController.java
+++ src/main/java/com/rionass/demonstration/student/StudentController.java
@@ -1,8 +1,7 @@
 package com.rionass.demonstration.student;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,5 +21,25 @@ public class StudentController {
     @GetMapping
     public List<Student> getStudent() {
         return studentService.getStudent();
+    }
+
+    //Specify EndPoint Post
+    @PostMapping
+    public void registerNewStudent(@RequestBody Student student){
+        studentService.addNewStudent(student);
+    }
+
+    @DeleteMapping(path = "{studentId}")
+    public void deleteStudent(@PathVariable("studentId") Long studentId){
+        studentService.deleteStudent(studentId);
+    }
+
+    @PutMapping(path = "{studentId}")
+    public void updateStudent(
+            @PathVariable("studentId") Long studentId,
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String email){
+
+        studentService.updateStudent(studentId, name, email);
     }
 }
